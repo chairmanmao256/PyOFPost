97bfb995634f0e92e2e67ac1679879d4c401036d
chairmanmao <1799729942@qq.com>
2025-01-07 10:25:41 +0800

    multiple-functions-added

--- pyofpost/FoamCase.py
+++ pyofpost/FoamCase.py
@@ -5,9 +5,15 @@ latter ML applications.
 '''
 
 import numpy as np
+import pandas as pd
 import re
 import os
 from scipy.interpolate import griddata
+
+# some helper functions
+def point2CellInterp():
+    pass
+
 
 class FoamTimeSave:
     def __init__(self, case_path, time):
@@ -87,18 +93,27 @@ class FoamTimeSave:
             elif "uniformValue" in self.fields[name]:
                 Warning("Writing uniform value to npy file")
                 np.save(os.path.join(output_path, name), self.fields[name]["uniformValue"])
+                
+    def build_df(self):
+        '''
+        ## Summary
+        Build a pd.DataFrame object for all the scalar field data.
+        '''
+        df_dict = {item: self.fields[item]["data"] for item in self.fields.keys() if self.fields[item]["type"]=="volScalarField"}
+        return pd.DataFrame(df_dict)
     
-
 class RefField:
     '''
     ## Description
     The instance of RefField class maintains the high-fidelity data for reference.
     '''
-    def __init__(self, RefPath, name):
+    def __init__(self, RefPath, name, onlyUx = False, readK = False):
         '''
         '''
         self.RefPath = RefPath
         self.name = name
+        self.onlyUx = onlyUx
+        self.readK = readK
         self.read_field()
         
     def read_field(self):
@@ -106,10 +121,12 @@ class RefField:
         self.fields["xx"] = np.genfromtxt(os.path.join(self.RefPath, self.name+"_x.txt"), skip_header=1)
         self.fields["yy"] = np.genfromtxt(os.path.join(self.RefPath, self.name+"_y.txt"), skip_header=1)
         self.fields["um"] = np.genfromtxt(os.path.join(self.RefPath, self.name+"_um.txt"),skip_header=1)
-        self.fields["vm"] = np.genfromtxt(os.path.join(self.RefPath, self.name+"_vm.txt"),skip_header=1) 
-        self.fields["k"] = 0.5*np.genfromtxt(os.path.join(self.RefPath, self.name+"_uu.txt"),skip_header=1)\
-                        + 0.5*np.genfromtxt(os.path.join(self.RefPath, self.name+"_vv.txt"),skip_header=1)\
-                        + 0.5*np.genfromtxt(os.path.join(self.RefPath, self.name+"_ww.txt"),skip_header=1)
+        if not self.onlyUx:
+            self.fields["vm"] = np.genfromtxt(os.path.join(self.RefPath, self.name+"_vm.txt"),skip_header=1) 
+            if self.readK:
+                self.fields["k"] = 0.5*np.genfromtxt(os.path.join(self.RefPath, self.name+"_uu.txt"),skip_header=1)\
+                                + 0.5*np.genfromtxt(os.path.join(self.RefPath, self.name+"_vv.txt"),skip_header=1)\
+                                + 0.5*np.genfromtxt(os.path.join(self.RefPath, self.name+"_ww.txt"),skip_header=1)
         
 class FoamLineComparison:
     '''
@@ -118,7 +135,7 @@ class FoamLineComparison:
     griddata to interploate the data to the reference line.
     '''
     def __init__(self, case_paths: list, times:list, names:list, RefPath:str, RefName: str,
-                 lines: dict):
+                 lines: dict, readK = False, readRef = True):
         '''
         ## Example input:
         FoamLineComparison(["/path/to/CBFS-CND", "/path/to/CBFS-NN"], [10000, 10000], ["CND", "NN"],
@@ -139,15 +156,19 @@ class FoamLineComparison:
         
         self.case_paths = case_paths
         self.times = times
-        self.RefPath = RefPath
+        if readRef:
+            self.RefPath = RefPath
         self.RefName = RefName
+        self.readK = readK
+        self.readRef = readRef
         
         self.cases = {}
         for i, name in enumerate(names):
             self.cases[name] = FoamTimeSave(case_paths[i], times[i])
         
         self.lines = lines
-        self.RefCase = RefField(RefPath, RefName)
+        if readRef:
+            self.RefCase = RefField(RefPath, RefName,readK=readK)
         self.cases = {}
         for path, time, name in zip(case_paths, times, names):
             self.cases[name] = FoamTimeSave(path, time)
@@ -166,6 +187,8 @@ class FoamLineComparison:
             yy = np.linspace(ls[1], le[1], npCase)
             self.lineData[key] = {}
             
+            k_success = True
+            
             for name in self.cases.keys():
                 self.lineData[key][name] = {}
                 self.lineData[key][name]["xx"] = xx
@@ -178,31 +201,122 @@ class FoamLineComparison:
                                                      self.cases[name].fields["Cy"]["data"]),
                                                      self.cases[name].fields["U"]["data"][:,1],
                                                      (xx, yy), method='linear')
-                try:
-                    self.lineData[key][name]["k"] = griddata((self.cases[name].fields["Cx"]["data"],
-                                                        self.cases[name].fields["Cy"]["data"]),
-                                                        self.cases[name].fields["k"]["data"],
-                                                        (xx, yy), method='linear')
-                except:
-                    self.lineData[key][name]["k"] = None
-                    print("The k field is not present in the dataset: {}".format(name))
-                
-            self.lineData[key]["Ref"] = {}
-            self.lineData[key]["Ref"]["xx"] = xx[::refSkip]
-            self.lineData[key]["Ref"]["yy"] = yy[::refSkip]
-            self.lineData[key]["Ref"]["u"] = griddata((self.RefCase.fields["xx"],
-                                                  self.RefCase.fields["yy"]),
-                                                  self.RefCase.fields["um"],
-                                                  (xx[::refSkip], yy[::refSkip]), method='linear')
-            self.lineData[key]["Ref"]["v"] = griddata((self.RefCase.fields["xx"],
-                                                  self.RefCase.fields["yy"]),
-                                                  self.RefCase.fields["vm"],
-                                                  (xx[::refSkip], yy[::refSkip]), method='linear')
-            self.lineData[key]["Ref"]["k"] = griddata((self.RefCase.fields["xx"],
-                                                  self.RefCase.fields["yy"]),
-                                                  self.RefCase.fields["k"],
-                                                  (xx[::refSkip], yy[::refSkip]), method='linear')
+                if self.readK:
+                    try:
+                        self.lineData[key][name]["k"] = griddata((self.cases[name].fields["Cx"]["data"],
+                                                            self.cases[name].fields["Cy"]["data"]),
+                                                            self.cases[name].fields["k"]["data"],
+                                                            (xx, yy), method='linear')
+                    except:
+                        self.lineData[key][name]["k"] = None
+                        print("The k field is not present in the dataset: {}".format(name))
+                        k_success=False
+                        
+            if self.readRef:
+                self.lineData[key]["Ref"] = {}
+                self.lineData[key]["Ref"]["xx"] = xx[::refSkip]
+                self.lineData[key]["Ref"]["yy"] = yy[::refSkip]
+                self.lineData[key]["Ref"]["u"] = griddata((self.RefCase.fields["xx"],
+                                                    self.RefCase.fields["yy"]),
+                                                    self.RefCase.fields["um"],
+                                                    (xx[::refSkip], yy[::refSkip]), method='linear')
+                self.lineData[key]["Ref"]["v"] = griddata((self.RefCase.fields["xx"],
+                                                    self.RefCase.fields["yy"]),
+                                                    self.RefCase.fields["vm"],
+                                                    (xx[::refSkip], yy[::refSkip]), method='linear')
+                if self.readK and k_success:
+                    self.lineData[key]["Ref"]["k"] = griddata((self.RefCase.fields["xx"],
+                                                        self.RefCase.fields["yy"]),
+                                                        self.RefCase.fields["k"],
+                                                        (xx[::refSkip], yy[::refSkip]), method='linear')
             
+class FoamDiff:
+    '''
+    ## Description
+    In this class, we use the FoamTimeSave class to read the simulation data, and use 
+    the RefField class to store the reference data. The simulation data is then interpolated
+    to the reference data's grid. The difference of the reference data and the interpolated
+    simulation data. 
+    '''
+    def __init__(self, case_paths: list, times:list, names:list, RefPath:str, RefName: str,
+                 box: dict):
+        '''
+        ## Example input:
+        FoamLineComparison(["/path/to/CBFS-CND", "/path/to/CBFS-NN"], [10000, 10000], ["CND", "NN"],
+                            RefPath="./path/to/LES", RefName="CBFS_13700", 
+                            lines={
+                                    "xmin": 0.0,
+                                    "xmax": 1.0,
+                                    "ymin": 0.0,
+                                    "ymax": 1.0,
+                                    "zmin": 0.0,
+                                    "zmax": 1.0
+                                   })
+        '''
+        assert (len(case_paths) == len(times) and len(case_paths) == len(names)),\
+            "The number of case paths, times, and names must be the same."
+        
+        self.case_paths = case_paths
+        self.times = times
+        self.RefPath = RefPath
+        self.RefName = RefName
+        
+        self.cases = {}
+        for i, name in enumerate(names):
+            self.cases[name] = FoamTimeSave(case_paths[i], times[i])
+        
+        self.box = box
+        self.RefCase = RefField(RefPath, RefName,readK=False)
+        self.cases = {}
+        for path, time, name in zip(case_paths, times, names):
+            self.cases[name] = FoamTimeSave(path, time)
             
+    def __call__(self):
+        self.interp()
+        self.boxMask()
+        self.Metrics()
+            
+    def interp(self):
+        '''
+        Interpolate the simulated data to the reference grid.
+        '''
+        self.diffs = {}
+        for name in self.cases.keys():
+            sim = self.cases[name]
+            interp_x = griddata((sim.fields["Cx"]["data"], sim.fields["Cy"]["data"]), sim.fields["U"]["data"][:,0],
+                              (self.RefCase.fields["xx"], self.RefCase.fields["yy"]), 
+                              method = "linear", fill_value=np.nan)
+            interp_y = griddata((sim.fields["Cx"]["data"], sim.fields["Cy"]["data"]), sim.fields["U"]["data"][:,1],
+                              (self.RefCase.fields["xx"], self.RefCase.fields["yy"]), 
+                              method = "linear", fill_value=np.nan)
+            
+            diff = np.vstack((interp_x - self.RefCase.fields["um"],
+                              interp_y - self.RefCase.fields["vm"])).T
+            
+            self.diffs[name] = diff
+            
+    def boxMask(self):
+        '''
+        Generate a mask for the cells that are in the box defined by the user.
+        '''
+        self.mask = ((self.RefCase.fields["xx"] <= self.box["xmax"]) & 
+                     (self.RefCase.fields["xx"] >= self.box["xmin"]) &
+                     (self.RefCase.fields["yy"] <= self.box["ymax"]) & 
+                     (self.RefCase.fields["yy"] >= self.box["ymin"]))
+    
+    def Metrics(self):
+        '''
+        Calculate the Metrics based on the diffs and the mask
+        '''
+        self.metrics = {k: [] for k in self.diffs.keys()}
+        for name in self.diffs.keys():
+            mse = (self.diffs[name]**2).sum(axis=1)
+            mse = np.where(self.mask, mse, np.nan)
+            self.metrics[name].append(np.nanmean(mse))
+            
+            mae = np.sqrt((self.diffs[name]**2).sum(axis=1))
+            mae = np.where(self.mask, mae, np.nan)
+            self.metrics[name].append(np.nanmean(mae))
+        self.metrics = pd.DataFrame(self.metrics, index=["MSE", "MAE"])
          
          
